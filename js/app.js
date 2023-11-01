a54c5bd4e7144e189e6e060f2af970bcde72c6db
Smith <samspike46@gmail.com>
2023-11-01 14:53:06 +0100

    register error

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener("load", () => {
         navigator.serviceWorker
-            .register("../cached_site.js",{scope:"/"})
+            .register("../cached_site.js",{scope:"/service_worker.github.io/"})
             .then(reg => console.log('Service worker: Registered'))
             .catch(err => console.log(`Service Worker: Error: ${err}`))
     })
